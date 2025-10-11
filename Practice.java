0538363941dec25a41aca52b24faf0b3602a27d4
dominic <aganachibuike@gmail.com>
2025-10-11 21:05:20 +0100

    simple code to practice

--- Practice.java
+++ Practice.java
@@ -6,7 +6,7 @@ public class Practice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("enter a fucking number : ");
+        System.out.println("enter a number : ");
         double num1 = scanner.nextDouble();
 
         System.out.println("what operation do you wish to do ");
